60181eb6b3b7e392530dec8af95a83af7a0d72b9
nicothebrush <nicola.riolini@gmail.com>
2016-01-07 18:10:52 +0100

    Start creatign inventory

--- base_log/__init__.py
+++ base_log/__init__.py
@@ -18,7 +18,6 @@
 #
 ###############################################################################
 from . import csv
-from . import wizard
 
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
