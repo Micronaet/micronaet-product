0578ce62f564a88050c7fb3e0ae97ded3ae9d28b
nicothebrush <nicola.riolini@gmail.com>
2016-01-09 16:29:51 +0100

    Change mode: inventory start create inventory and use button for load product!

--- csv_import_inventory/csv.py
+++ csv_import_inventory/csv.py
@@ -42,8 +42,123 @@ class StockInventory(orm.Model):
     ''' Product for link import log
     '''
     _inherit = 'stock.inventory'
-    
+
+    # -------------
+    # Button event:
+    # -------------
+    def action_import_product_from_csv(self, cr, uid, ids, context=None):
+        ''' Import detail button
+        '''
+        filename = '/home/administrator/photo/xls' # TODO parametrize
+        max_line = 10000
+        _logger.info('Start import from path: %s' % filename)
+        
+        # Pool used:
+        product_pool = self.pool.get('product.product')
+        log_pool = self.pool.get('product.product.importation')
+
+        inventory_proxy = self.browse(cr, uid, ids, context=context)[0] 
+        
+        # ---------------------------------------------------------------------
+        #                Open XLS document (first WS):
+        # ---------------------------------------------------------------------
+        # ----------------
+        # Read excel file:
+        # ----------------
+        try:
+            filename = os.path.join(filename, inventory_proxy.filename)
+            # from xlrd.sheet import ctype_text   
+            wb = xlrd.open_workbook(filename)
+            ws = wb.sheet_by_index(0)
+        except:
+            error = 'Error opening XLS file: %s' % (sys.exc_info(), )
+
+        # ----------------------------------
+        # Create import log for this import:
+        # ----------------------------------
+        log_id = log_pool.create(cr, uid, {
+            'name': wiz_proxy.comment or 'No comment',
+            'error': error,
+            # Extra info write at the end
+            }, context=context)
+        log_view['res_id'] = log_id # Save record ID to open after import
+        if error:
+            return log_view
+        
+        # ------------------------
+        # Create inventory header:
+        # ------------------------
+
+        error = annotation = ''
+        for i in range(0, max_line):
+            try:
+                row = ws.row(i) # generate error at end
+            except:
+                # Out of range error ends import:
+                annotation += _('Import end at line: %s\n') % i
+                break
+
+            try:
+                # Loop on colums (trace)
+                default_code = row[0].value
+                product_qty = row[1].value # TODO check if is float!
+                #TODO lot = row[2].value 
+                
+                # Search product with code:
+                if not default_code:
+                    error += _(
+                        'Error no code present in line: <b>%s</b></br>') % i
+                    continue # jump
+
+                product_ids = product_pool.search(cr, uid, [
+                    ('default_code', '=', default_code)], context=context)
+
+                if not product_ids:
+                    error += _(
+                        '%s. Error code not found, code: <b>%s</b></br>') % (
+                            i, default_code)
+                    continue # jump
+
+                elif len(product_ids) > 1:
+                    error += _(
+                        '''%s. Error more code (take first), 
+                            code: <b>%s</b></br>''') % (
+                                i, default_code)
+                data = {                
+                    'csv_import_id': log_id # link to log event
+                    
+                        
+                product_pool.write(cr, uid, product_ids[0], data, 
+                    context=context)
+                _logger.info('Update product code: %s' % default_code)            
+            except:
+                error += _('%s. Import error code: <b>%s</b> [%s]</br>') % (
+                    i, default_code, sys.exc_info())
+                    
+
+        # End operations:    
+        context['lang'] = current_lang
+        # Update lof with extra information:            
+        log_pool.write(cr, uid, log_id, {
+            'inventory_id': inventory_id, # Link
+            'error': error,
+            'note': '''
+                File: <b>%s</b></br>
+                Import note: <i>%s</i></br>
+                Operator note:</br><i>%s</i>
+                ''' % (
+                    wiz_proxy.name, 
+                    annotation, 
+                    wiz_proxy.note or ''),
+            }, context=context)
+
+        _logger.info('End import XLS product file: %s' % wiz_proxy.name)
+        return log_view
+        
+            
     _columns = {
+        'fielname': fields.char(
+            'Inventory filename', size=80), 
         'csv_import_id': fields.many2one('product.product.importation',
             'Log import', ondelete='set null'),
         }
