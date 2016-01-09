beb30f0e086f70cde9d980b9a2c0dda3a94f8273
nicothebrush <nicola.riolini@gmail.com>
2016-01-09 18:04:48 +0100

    [ADD] partner_product_partic: module for save in partner page list of particularity used for product (like code and description of partner, used in transcode report)

---
+++ partner_product_partic/partic.py
@@ -0,0 +1,66 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP module
+#    Copyright (C) 2010 Micronaet srl (<http://www.micronaet.it>) 
+#    
+#    Italian OpenERP Community (<http://www.openerp-italia.com>)
+#
+#############################################################################
+#
+#    OpenERP, Open Source Management Solution	
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import sys
+import os
+from openerp.osv import osv, fields
+from datetime import datetime, timedelta
+import logging
+
+
+_logger = logging.getLogger(__name__)
+
+
+class ResPartnerProductPartic(osv.osv):
+    ''' Add product partic obj
+    '''    
+    _inherit = 'res.partner.product.partic'
+
+    # -------------------------------------------------------------------------
+    #                                 Columns
+    # -------------------------------------------------------------------------
+    _columns = {
+        'partner_id': fields.many2one('res.partner', 'Partner'), 
+        'product_id': fields.many2one('product.product', 'Product', 
+            required=True), 
+        'partner_code': fields.char('Partner code', size=18, required=True), 
+        'partner_description': fields.char('Partner description', size=60,
+            help='Description in partner\'s language'
+            ),
+        }
+
+class ResPartner(osv.osv):
+    ''' Add product partic in partner
+    '''    
+    _inherit = 'res.partner'
+        
+    _columns = {
+        'partic_ids': fields.one2many(
+            'res.partner.product.partic', 'partner_id', 'Product partic.'), 
+        }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
