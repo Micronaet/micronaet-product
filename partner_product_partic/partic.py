ee8d31157ec113f4618a1ef1f13adc4f50d608dc
nicothebrush <nicola.riolini@gmail.com>
2016-01-14 16:55:51 +0100

    enlarge field

--- partner_product_partic/partic.py
+++ partner_product_partic/partic.py
@@ -44,7 +44,7 @@ class ResPartnerProductPartic(osv.osv):
     #                                 Columns
     # -------------------------------------------------------------------------
     _columns = {
-        'partner_code': fields.char('Partner code', size=18, required=True), 
+        'partner_code': fields.char('Partner code', size=40, required=True), 
         'partner_description': fields.char('Partner description', size=60,
             help='Description in partner\'s language'
             ),
