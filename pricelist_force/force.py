7949df50e6dcc04072ebcbf2efb868d7a8919990
nicothebrush <nicola.riolini@gmail.com>
2016-07-22 09:15:45 +0200

    nothing

--- pricelist_force/force.py
+++ pricelist_force/force.py
@@ -35,7 +35,7 @@ from openerp.tools import (DEFAULT_SERVER_DATE_FORMAT,
     DATETIME_FORMATS_MAP, 
     float_compare)
 
-
+ 
 _logger = logging.getLogger(__name__)
 
 class ProductPricelistVersion(orm.Model):
