24fdff1be03415121c0d623ff44d7c5a05187e94
nicothebrush <nicola.riolini@gmail.com>
2017-01-20 11:22:28 +0100

    add date with time

--- inventory_status_mrp/mrp_unload.py
+++ inventory_status_mrp/mrp_unload.py
@@ -84,7 +84,7 @@ class MrpProduction(orm.Model):
         
         #from_date = '2016-08-30'
         # After inventory date:
-        from_date = '2017-01-01'
+        from_date = '2017-01-01 00:00:00'
         
         mrp_ids = self.search(cr, uid, [        
             # State filter:
