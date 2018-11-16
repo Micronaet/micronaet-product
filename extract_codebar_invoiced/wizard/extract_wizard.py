e70558b0728a9d012ea5e6509239c67ff10adc78
nicothebrush <nicola.riolini@gmail.com>
2018-11-16 17:01:10 +0100

    extend report also in detail mode

--- extract_codebar_invoiced/wizard/extract_wizard.py
+++ extract_codebar_invoiced/wizard/extract_wizard.py
@@ -53,6 +53,15 @@ class AccountInvoiceExtractCodebarWizard(orm.TransientModel):
     def action_done(self, cr, uid, ids, context=None):
         ''' Event for button done
         '''
+        def safe_eval_field(browse, field):
+            ''' Return save eval for field (if not present)
+            '''
+            try: 
+                return browse.__getattribute__(field) or ''
+            except:
+                return ''
+            
+            
         if context is None: 
             context = {}        
         
@@ -65,6 +74,7 @@ class AccountInvoiceExtractCodebarWizard(orm.TransientModel):
         # Generate domain:
         domain = []
         filter_name = ''
+        mode = wiz_browse.mode
         partner = wiz_browse.partner_id
         if partner:
             domain.append(('invoice_id.partner_id', '=', partner.id))
@@ -91,13 +101,34 @@ class AccountInvoiceExtractCodebarWizard(orm.TransientModel):
         # Start Excel file:        
         # ---------------------------------------------------------------------
         ws_name = 'EAN'
-        header = [
-            'Codice prodotto', 
-            'Nome prodotto', 
-            'EAN Pack',
-            'EAN Singolo',
-            ]
-        width = [20, 40, 15, 15]
+        if mode == 'detailed':
+            header = [
+                'Articolo', 
+                'Descrizione', 
+                'Descrizione dettagliata',
+                'Telaio',
+                'Tessuto',
+                'Pz. x Scatola',
+                'Peso netto',
+                'Peso lordo',
+                'Pz. per pallet',
+                'Pz. per camion',
+                'Dimensione',
+                'Altezza seduta',
+                'Dimensioni scatola',
+                'Volume CMB',
+                'EAN Pack',
+                'EAN Singolo',
+                ]
+            width = [20, 40, 40, 25, 25, 7, 7, 7, 7, 7, 15, 7, 15, 10, 15, 15]
+        else:
+            header = [
+                'Articolo', 
+                'Descrizione', 
+                'EAN Pack',
+                'EAN Singolo',
+                ]
+            width = [20, 40, 15, 15]
         
         ws = excel_pool.create_worksheet(name=ws_name)
         excel_pool.column_width(ws_name, width)
@@ -126,17 +157,40 @@ class AccountInvoiceExtractCodebarWizard(orm.TransientModel):
         
         row += 1
         for product in sorted(product_db, key=lambda x: x.default_code):
-            try: 
-                ean13_mono = product.ean13_mono
-            except:
-                ean13_mono = ''
-
-            line = [
-                product.default_code or '',
-                product.name or '',
-                product.ean13,
-                ean13_mono,
-                ]
+            if mode == 'detailed':
+                line = [
+                    safe_eval_field(product, 'default_code'),
+                    safe_eval_field(product, 'name'),
+                    safe_eval_field(product, 'extra_description'),
+                    safe_eval_field(product, 'telaio'),
+                    safe_eval_field(product, 'fabric'),
+                    safe_eval_field(product, 'q_x_pack'),
+                    safe_eval_field(product, 'weight_net'),
+                    safe_eval_field(product, 'weight'),
+                    safe_eval_field(product, 'item_x_pallet'),
+                    safe_eval_field(product, 'item_x_camion'),
+                    ('%s x %s x %s' % (
+                        safe_eval_field(product, 'height'), 
+                        safe_eval_field(product, 'width'), 
+                        safe_eval_field(product, 'length'),
+                        )).replace(' x  x ', ''),
+                    '', #TODO 'Altezza seduta',
+                    ('%s x %s x %s' % (
+                        safe_eval_field(product, 'pack_l'), 
+                        safe_eval_field(product, 'pack_h'), 
+                        safe_eval_field(product, 'pack_p'),
+                        )).replace(' x  x ', ''),
+                    safe_eval_field(product, 'volume'),
+                    safe_eval_field(product, 'ean13'),
+                    safe_eval_field(product, 'ean13_mono'),
+                    ]
+            else:
+                line = [
+                    safe_eval_field(product, 'default_code'),
+                    safe_eval_field(product, 'name'),
+                    safe_eval_field(product, 'ean13'),
+                    safe_eval_field(product, 'ean13_mono'),
+                    ]
             excel_pool.write_xls_line(
                 ws_name, row, line, default_format=f_text)
             row += 1
@@ -147,6 +201,14 @@ class AccountInvoiceExtractCodebarWizard(orm.TransientModel):
             'res.partner', 'Partner', help='Partner selected', required=True),
         'from_date': fields.date('From date >='),    
         'to_date': fields.date('To date <='),
+        'mode': fields.selection([
+            ('ean', 'Solo EAN'),
+            ('detailed', 'Dettagliata'),
+            ], u'Modalità'),
         }
-        
+
+    _defaults = {
+        # Default value:
+        'mode': lambda *x: 'detailed',
+        }        
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
