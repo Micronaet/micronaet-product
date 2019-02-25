88cc1eb64944f53a004aebb32d0ab73e6f1a8844
nicothebrush <nicola.riolini@gmail.com>
2019-02-25 13:08:22 +0100

    correct assign status and qty

--- inventory_status_assign_wizard/assign_wizard.py
+++ inventory_status_assign_wizard/assign_wizard.py
@@ -48,37 +48,122 @@ class SaleOrderLine(orm.Model):
     
     _inherit = 'sale.order.line'
 
+    def restore_stock_status_user_value(
+            self, cr, uid, no_inventory_status, context=None):
+        ''' Update with previous value
+        '''
+        return self.pool.get('res.users').write(
+            cr, uid, [uid], {
+                'no_inventory_status': no_inventory_status,
+                }, context=context)
+
+        
     def return_view_assign_wizard(self, cr, uid, ids, context=None):
         ''' Open wizard view:
         '''
-        model_pool = self.pool.get('ir.model.data')
-        view_id = model_pool.get_object_reference(
-            cr, uid,
-            'inventory_status_assign_wizard', 
-            'sale_order_line_assign_stock_wizard_view')[1]
+        wiz_pool = self.pool.get('sale.order.line.assign.stock.wizard')
 
-        # Check previsional order:
-        line_proxy = self.browse(cr, uid, ids, context=context)[0]
-        order = line_proxy.order_id
+        # Activate stock status:
+        user_pool = self.pool.get('res.users')
+        user = user_pool.browse(cr, uid, uid, context=context)
+        no_inventory_status = user.no_inventory_status
+        user_pool.write(cr, uid, [uid], {
+            'no_inventory_status': False,
+            }, context=context)    
+            
+        # ---------------------------------------------------------------------
+        #                             Check data:
+        # ---------------------------------------------------------------------
+        # A. Check previsional order:
+        line = self.browse(cr, uid, ids, context=context)[0]
+        order = line.order_id
         if order.previsional:
+            self.restore_stock_status_user_value(
+                cr, uid, no_inventory_status, context=context)
             raise osv.except_osv(
                 _('Errore'), 
                 _('''Ordine previsionale, non permessa una assegnazione da 
                      magazzino in quando viene fatto per caricare il magazzino
                      '''))
+        
+        # B. State of order:             
         if order.state not in ('manual', 'progress'):
+            self.restore_stock_status_user_value(
+                cr, uid, no_inventory_status, context=context)
             raise osv.except_osv(
                 _('Errore'), 
                 _('''Ordine non nel corretto stato:
                      solo gli ordini attivi non chiusi possono avere 
                      assegnazioni da magazzino.
                      '''))
-    
-        wiz_pool = self.pool.get('sale.order.line.assign.stock.wizard')
+
+        # C. Available in stock:    
+        product = line.product_id
+        available = product.mx_net_mrp_qty - product.mx_mrp_b_locked
+        if available <= 0.0:
+            self.restore_stock_status_user_value(
+                cr, uid, no_inventory_status, context=context)
+            raise osv.except_osv(
+                _(u'Errore'), 
+                _(u'Il prodotto %s non ha disponibilità a magazzino!' % (
+                    product.default_code or product.name or '?'
+                    )),
+                )
+        
+        # D. Remain positive:
+        oc_qty = line.product_uom_qty
+        delivery_qty = line.delivered_qty
+        remain = oc_qty - delivery_qty
+
+        if 'product_uom_maked_sync_qty' in line._columns:
+            maked = line.product_uom_maked_sync_qty
+            if maked > delivery_qty: # Has maked to delivery:
+                remain -= maked - delivery_qty
+
+        if remain <= 0:
+            self.restore_stock_status_user_value(
+                cr, uid, no_inventory_status, context=context)
+            raise osv.except_osv(
+                _(u'Errore'), 
+                _(u'Al prodotto %s non servono assegnazioni di magazzino!' % (
+                    product.default_code or product.name or '?'
+                    )),
+                )
+
+        # ---------------------------------------------------------------------
+        # Create record for wizard and open:
+        # ---------------------------------------------------------------------
+        # Default assignement:
+        if remain >= available:
+            new_assigned_qty = available
+        else:
+            new_assigned_qty = remain   
+            
         wiz_id = wiz_pool.create(cr, uid, {
+            'new_assigned_qty': new_assigned_qty,
             'line_id': ids[0],
+            'status': '''
+                Attualmente assegnate (prodotte o manuali): <b>%s</b><br/>
+                Assegnabili per il prodotto: <b>%s</b><br/>
+                <i>(Assegnate manualmente in origine: %s)</i>
+                <i>(Mancanti: %s)</i>
+                ''' % (
+                    line.mx_locked_qty,
+                    available,
+                    line.mx_assigned_qty,
+                    remain,
+                    )
             }, context=context)
 
+        # Get and return correct view:
+        model_pool = self.pool.get('ir.model.data')
+        view_id = model_pool.get_object_reference(
+            cr, uid,
+            'inventory_status_assign_wizard', 
+            'sale_order_line_assign_stock_wizard_view')[1]
+
+        self.restore_stock_status_user_value(
+            cr, uid, no_inventory_status, context=context)
         return {
             'type': 'ir.actions.act_window',
             'name': _('Assegna q. magazzino'),
@@ -86,31 +171,44 @@ class SaleOrderLine(orm.Model):
             'view_mode': 'form',
             'res_id': wiz_id,
             'res_model': 'sale.order.line.assign.stock.wizard',
-            'view_id': view_id, # False
+            'view_id': view_id,
             'views': [(view_id, 'form')],
             'domain': [],
             'context': context,
             'target': 'new',
             'nodestroy': False,
-            }
-            
+            }            
 
 class SaleOrderLineAssignStockWizard(orm.TransientModel):
     ''' Wizard for stock wizard
     '''
     _name = 'sale.order.line.assign.stock.wizard'
 
-    # --------------------
+    # -------------------------------------------------------------------------
     # Wizard button event:
-    # --------------------
-    def action_done(self, cr, uid, ids, context=None):
+    # -------------------------------------------------------------------------
+    def action_remove_qty(self, cr, uid, ids, context=None):
+        ''' Event for button done
+        '''
+        if context is None: 
+            context = {}
+
+        # Remove assignement:
+        line_pool = self.pool.get('sale.order.line')    
+        wiz_browse = self.browse(cr, uid, ids, context=context)[0]
+        line_id = wiz_browse.line_id
+
+        return line_pool.write(cr, uid, line_id.id, {
+            'mx_assigned_qty': 0,
+            }, context=context)
+        
+    def action_assign_qty(self, cr, uid, ids, context=None):
         ''' Event for button done
         '''
         if context is None: 
             context = {}
             
         line_pool = self.pool.get('sale.order.line')    
-        
         wiz_browse = self.browse(cr, uid, ids, context=context)[0]
         
         # Parameters:
@@ -118,57 +216,14 @@ class SaleOrderLineAssignStockWizard(orm.TransientModel):
         new_assigned_qty = wiz_browse.new_assigned_qty
         
         # Update new assignement:
-        line_pool.write(cr, uid, line_id.id, {
+        return line_pool.write(cr, uid, line_id.id, {
             'mx_assigned_qty': new_assigned_qty,
             }, context=context)
-        
-        return {
-            'type': 'ir.actions.act_window_close'
-            }
 
-    def _get_status_line(self, cr, uid, context=None):
-        ''' Fields function for calculate 
-        '''
-        if context is None:
-            context = {}
-        line_pool = self.pool.get('sale.order.line')
-        
-        line_id = context.get('default_line_id', False)
-        if not line_id:
-            raise osv.except_osv(
-                _('Errore'), 
-                _('Nessuna riga ordine presente'),
-                )
-        line = line_pool.browse(cr, uid, line_id, context=context)
-        product = line.product_id
-        available = product.mx_net_mrp_qty - product.mx_mrp_b_locked
-        if available <= 0.0:
-            raise osv.except_osv(
-                _(u'Errore'), 
-                _(u'Il prodotto non ha quantità disponibili a magazzino!' % (
-                    product.default_code or product.name or '?'
-                    )),
-                )
-        
-        return '''
-            Attualmente rimaste assegnate: <b>%s</b><br/>
-            Assegnabili per il prodotto: <b>%s</b><br/>
-            <i>(Assegnate manualmente in origine: %s)</i>
-            ''' % (
-                line.mx_locked_qty,
-                available,
-                line.mx_assigned_qty,
-                )
-        
     _columns = {
         'line_id': fields.many2one(
             'sale.order.line', 'Sale line'),
         'status': fields.text('Stato riga'),
         'new_assigned_qty': fields.float('Nuova assegnazione', digits=(16, 2)),    
         }
-
-    _defaults = {
-        'status': lambda s, cr, uid, ctx: s._get_status_line(cr, uid, ctx),
-        }    
-
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
