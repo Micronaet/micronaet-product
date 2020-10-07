ced1f8becdf3b760753346103d7d4e47de7b94ac
nicothebrush <nicola.riolini@gmail.com>
2020-10-07 21:35:44 +0200

    generic bugfix (fast message)

--- edi_extract/wizard/extract_wizard.py
+++ edi_extract/wizard/extract_wizard.py
@@ -67,7 +67,8 @@ class EdiProductProductExtractWizard(orm.Model):
 
         # Parameters:
         # Album / image:
-        url_image_mask = 'http://my.%s.it/upload/EDI/%%s/%%s \n' % cr.dbname
+        url_image_mask = 'http://my.%s.it/upload/EDI/%%s/%%s \n' % \
+                         cr.dbname.lower()
 
         langs = ['it_IT', 'en_US']
         if context is None:
