adf24b040b2f7a1ef760ee40d9bcd1cade55ce77
nicothebrush <nicola.riolini@gmail.com>
2020-10-20 17:05:11 +0200

    view for hidden product

--- inventory_field/inventory.py
+++ inventory_field/inventory.py
@@ -30,39 +30,40 @@ from openerp import SUPERUSER_ID, api
 from openerp import tools
 from openerp.tools.translate import _
 from openerp.tools.float_utils import float_round as round
-from openerp.tools import (DEFAULT_SERVER_DATE_FORMAT, 
-    DEFAULT_SERVER_DATETIME_FORMAT, 
-    DATETIME_FORMATS_MAP, 
+from openerp.tools import (DEFAULT_SERVER_DATE_FORMAT,
+    DEFAULT_SERVER_DATETIME_FORMAT,
+    DATETIME_FORMATS_MAP,
     float_compare)
 
 
 _logger = logging.getLogger(__name__)
 
+
 class ProductProductInventoryCategory(orm.Model):
     """ Model name: ProductProductInventoryCategory
     """
-    
+
     _name = 'product.product.inventory.category'
     _description = 'Inventory category'
-    
+
     def force_no_code_category(self, cr, uid, ids, context=None):
         ''' Force all no code to this category
         '''
         product_pool = self.pool.get('product.product')
-        
+
         current_proxy = self.browse(cr, uid, ids, context=context)[0]
         product_ids = product_pool.search(cr, uid, [
             ('default_code', '=', False)], context=context)
         product_pool.write(cr, uid, product_ids, {
             'inventory_category_id': current_proxy.id,
-            }, context=context)    
-        return True    
-        
+            }, context=context)
+        return True
+
     def force_code_category(self, cr, uid, ids, context=None):
         ''' Force product category with code in text field
         '''
         product_pool = self.pool.get('product.product')
-        
+
         current_proxy = self.browse(cr, uid, ids, context=context)[0]
         code = current_proxy.code
         code_list = code.split('\n')
@@ -70,33 +71,33 @@ class ProductProductInventoryCategory(orm.Model):
             ('default_code', 'in', code_list)], context=context)
         product_pool.write(cr, uid, product_ids, {
             'inventory_category_id': current_proxy.id,
-            }, context=context)    
-        return True    
-        
+            }, context=context)
+        return True
+
     _columns = {
         'name': fields.char(
             'Name', size=64, required=True),
-        'note': fields.text('Note'),    
-        'code': fields.text('Force code'),         
+        'note': fields.text('Note'),
+        'code': fields.text('Force code'),
         }
+
 
 class ProductProduct(orm.Model):
     ''' Link product to inventory purchase order
     '''
     _inherit = 'product.product'
-    
+
     _columns = {
         # TODO No more use:
         'inventory_start': fields.float(
             'Inventory start', digits=(16, 3)),
         'inventory_delta': fields.float(
-            'Inventory delta', digits=(16, 3), 
+            'Inventory delta', digits=(16, 3),
             help='Delta inventory for post correction retroactive'),
-        'inventory_date': fields.date('Inventory date'),    
-        
+        'inventory_date': fields.date('Inventory date'),
+
         # XXX Inventory report (keep in isolated module?)
         'inventory_category_id': fields.many2one(
             'product.product.inventory.category', 'Inventory category'),
-        'inventory_excluded': fields.boolean('Inventory excluded'),    
+        'inventory_excluded': fields.boolean('Inventory excluded'),
         }
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
