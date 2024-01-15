6979a070f8994c528622d3b36a64e478d5d0e1e6
nicothebrush <nicola.riolini@gmail.com>
2024-01-15 10:44:23 +0100

    generic bugfix (fast message)

--- product_easy_label/wizard/wizard_label_report.py
+++ product_easy_label/wizard/wizard_label_report.py
@@ -32,43 +32,46 @@ from openerp import SUPERUSER_ID, api
 from openerp import tools
 from openerp.tools.translate import _
 from openerp.tools.float_utils import float_round as round
-from openerp.tools import (DEFAULT_SERVER_DATE_FORMAT, 
-    DEFAULT_SERVER_DATETIME_FORMAT, 
-    DATETIME_FORMATS_MAP, 
+from openerp.tools import (DEFAULT_SERVER_DATE_FORMAT,
+    DEFAULT_SERVER_DATETIME_FORMAT,
+    DATETIME_FORMATS_MAP,
     float_compare)
 
 
 _logger = logging.getLogger(__name__)
 
+
 class SaleOrderGeneralReportWizard(orm.TransientModel):
-    ''' Procurements depend on sale
-    '''    
+    """ Procurements depend on sale
+    """
     _name = 'sale.order.general.report.wizard'
     _description = 'Sale general wizard'
-    
+
     # --------------
     # Button events:
     # --------------
     def print_report(self, cr, uid, ids, context=None):
-        ''' Redirect to report passing parameters
-        ''' 
+        """ Redirect to report passing parameters
+        """
         wiz_proxy = self.browse(cr, uid, ids)[0]
-            
+
         datas = {}
-        datas['wizard'] = True # started from wizard
-        
+        datas['wizard'] = True  # started from wizard
+
         if wiz_proxy.report_type == 'deadlined':
             report_name = 'mx_order_list_report'
-        else: # 'line'    
+        else:  # 'line'
             report_name = 'mx_order_list_line_report'
 
+        datas['statistic_category_id'] = \
+            wiz_proxy.statistic_category_id.id or False
         datas['partner_id'] = wiz_proxy.partner_id.id or False
         datas['fiscal_position'] = wiz_proxy.fiscal_position
         datas['from_date'] = wiz_proxy.from_date or False
         datas['to_date'] = wiz_proxy.to_date or False
         datas['from_deadline'] = wiz_proxy.from_deadline or False
         datas['to_deadline'] = wiz_proxy.to_deadline or False
-        #datas['only_remain'] = wiz_proxy.only_remain
+        # datas['only_remain'] = wiz_proxy.only_remain
 
         return {
             'type': 'ir.actions.report.xml',
@@ -85,22 +88,24 @@ class SaleOrderGeneralReportWizard(orm.TransientModel):
         'report_type': fields.selection([
             ('deadlined', 'Order deadline'),
             ('line', 'Order line deadline'),
-            #('grouped', 'Order grouped by frame'),
+            # ('grouped', 'Order grouped by frame'),
             ], 'Report type', required=True),
+        'statistic_category_id': fields.many2one(
+            'statistic.category', 'Categoria statistica'),
         'partner_id': fields.many2one('res.partner', 'Partner'),
-        'only_remain':fields.boolean('Only remain', 
+        'only_remain': fields.boolean(
+            'Only remain',
             help='Show only element to procuce'),
-        
+
         'from_date': fields.date('From', help='Date >='),
         'to_date': fields.date('To', help='Date <'),
         'from_deadline': fields.date('From deadline', help='Date deadline >='),
         'to_deadline': fields.date('To deadline', help='Date deadline <'),
         }
-        
+
     _defaults = {
         'fiscal_position': lambda *x: 'all',
         'report_type': lambda *x: 'deadlined',
         'only_remain': lambda *x: True,
-        #'to_date': datetime.now().strftime(DEFAULT_SERVER_DATE_FORMAT),
+        # 'to_date': datetime.now().strftime(DEFAULT_SERVER_DATE_FORMAT),
         }
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
